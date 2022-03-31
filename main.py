26733f02f460f73a8022ea5250317d0e63fd1b63
JamesKunstle <52969093+JamesKunstle@users.noreply.github.com>
2022-03-31 10:32:08 -0400

    added multi-regex support
    Signed-off-by: JamesKunstle <jkunstle@bu.edu>

--- main.py
+++ main.py
@@ -14,18 +14,32 @@ class Regex(AddOn):
             self.set_message("Please select at least one document")
             return
 
-        regex = self.data["regex"]
-        pattern = re.compile(regex)
-
+        # assumes that the regex pattern list is at least of length 1
+        if(len(self.data["regex"]) < 1):
+            self.set_message("Please provide at least one regular expression.")
+            return
+        
         with open("matches.csv", "w+") as file_:
-            writer = csv.writer(file_)
-            writer.writerow(["match", "url"])
 
-            for document in self.client.documents.list(id__in=self.documents):
-                writer.writerows(
-                    [m, document.canonical_url]
-                    for m in pattern.findall(document.full_text)
-                )
+            writer = csv.writer(file_)
+            writer.writerow(["pattern", "match", "url"])
+
+            # for each pattern supplied
+            for regex_pattern in self.data["regex"]:
+                print("Pattern: " + regex_pattern)
+                pattern = re.compile(regex_pattern)
+
+                for document in self.client.documents.list(id__in=self.documents):
+                    writer.writerows(
+                        [regex_pattern, m, document.canonical_url]
+                        for m in pattern.findall(document.full_text)
+                    )
+                    
+            print("CSV Document Contents:")
+            # go to the beginning of the file
+            file_.seek(0)
+            # print the file contents
+            print(file_.read())
 
             self.upload_file(file_)
 
